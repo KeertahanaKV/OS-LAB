93f12fd2159c5c668ef6335718bf4f80bb6f1422
Keerthana K V <167847848+KeertahanaKV@users.noreply.github.com>
2024-08-03 20:39:59 +0530

    Create 1a.sh

---
+++ 1a.sh
@@ -0,0 +1,30 @@
+# Read the number
+echo "Enter a number:"
+read num
+
+# Reverse the number
+reverse=$(echo $num | rev)
+
+# Check if the original number and reversed number are the same
+if [ "$num" == "$reverse" ]; then
+    echo "$num is a palindrome."
+else
+    echo "$num is not a palindrome."
+fi
+
+: <<'END_COMMENT'
+echo "read number"
+read num
+rev=0
+original=$num
+while [ "$num" -gt 0 ]
+do
+        rem=$((num%10))
+        rev=$((rev*10+rem))
+        num=$((num/10))
+done
+if [ "$original" -eq "$rev" ];then
+        echo "palindrome"
+else
+        echo "not palindrome"
+fi
