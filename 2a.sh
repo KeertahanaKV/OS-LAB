7bff312f7bf51d761285b258596e8fe4647f0931
Keerthana K V <167847848+KeertahanaKV@users.noreply.github.com>
2024-08-03 22:14:14 +0530

    Update 2a.sh

--- 2a.sh
+++ 2a.sh
@@ -1,19 +1,23 @@
-echo "Enter the lower bound"
+#!/bin/bash
+
+echo "Enter the lower bound:"
 read lb
-echo "Enter the upper bound"
+echo "Enter the upper bound:"
 read ub
-flag=0
-for((i=lb;i<=ub;i++))
-do
-  for((j=2;j<=i/2;j++))
-  do
-    if [ $((i%j)) -eq 0 ]; then
-    flag=1
-    break
+
+# Loop through each number in the range
+for ((i = lb; i <= ub; i++)); do
+    if [ $i -lt 2 ]; then
+        continue
     fi
-  done
-  if [ $flag -eq 0 ]; then
-  echo -ne " $i"
-   fi
-   flag=0
+    flag=0
+    for ((j = 2; j <= i / 2; j++)); do
+        if [ $((i % j)) -eq 0 ]; then
+            flag=1
+            break
+        fi
+    done
+    if [ $flag -eq 0 ]; then
+        echo "$i"
+    fi
 done
